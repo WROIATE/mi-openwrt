2c03cda722befea79e0db1757437ed0e6f5a905e
Jarao <44677306+WROIATE@users.noreply.github.com>
2020-12-20 14:13:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,22 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config
+popd
+
+# Add luci-app-ustb
+git clone https://github.com/WROIATE/luci-app-ustb
+
+pushd package/lean/default-settings/files
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" zzz-default-settings
+sed -i "/uci commit system/a\uci set dhcp.@dnsmasq[0].filter_aaaa='0'" zzz-default-settings
+sed -i "/dhcp.@dnsmasq/a\uci commit dhcp" zzz-default-settings
+sed -i "/-j REDIRECT --to-ports 53/d" zzz-default-settings
+sed -i "/REDIRECT --to-ports 53/a\echo '# iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53' >> /etc/firewall.user" zzz-default-settings
+sed -i "/exit 0/i\uci set network.globals.ula_prefix='dead:2333:6666::/48'" zzz-default-settings
+sed -i "/exit 0/i\uci commit network" zzz-default-settings
+popd
